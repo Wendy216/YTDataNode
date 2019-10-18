a67d94d55ef491d54b191bbf31a1706c39376307
Wendy216 <38710168+Wendy216@users.noreply.github.com>
2019-10-18 16:34:05 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -13,7 +13,7 @@ mkdir ${DIRNAME}
 if [ "$1" = "linux" ];then
 docker run  --rm -v $GOPATH:/go -w /go/src/github.com/yottachain/YTDataNode/ golang go build -o ${FULLNAME} ${MAINFILE}
 #docker run  --rm -v $GOPATH:/go -w /var/lib/jenkins/workspace/YTDataNode/src/github.com/yottachain/YTDataNode/ golang go build -o ${FULLNAME} ${MAINFILE}
-docker run  --rm -v `pwd`/$DIRNAME/:/src upx upx -9 /src/$FILENAME
+#docker run  --rm -v `pwd`/$DIRNAME/:/src upx upx -9 /src/$FILENAME
 elif [ "$1" = "windows" ];then
 wget http://10.211.55.3:6001/${FILENAME}.exe -o ${FULLNAME}.exe
 # CGO_ENABLED=1 GOOS=$OS GOARCH=$ARCH CXX=/Users/mac/go/src/github.com/yottachain/YTDataNode/gcc/g++-linux-amd64 CC=/Users/mac/go/src/github.com/yottachain/YTDataNode/gcc/gcc-linux-amd64 go build -ldflags "-s -w" -o ${FULLNAME} ${MAINFILE}
