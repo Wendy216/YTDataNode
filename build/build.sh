3c8d891b0c98330da02c39c14ae3ba914a08ed1e
Wendy216 <38710168+Wendy216@users.noreply.github.com>
2019-10-21 17:15:59 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -11,7 +11,8 @@ FULLNAME=${DIRNAME}/${FILENAME}
 mkdir -p ${DIRNAME}
 echo $DIRNAME
 if [ "$1" = "linux" ];then
-docker run --rm -v $GOPATH:/go -w /go/src/github.com/yottachain/YTDataNode/ golang go build -o ${FULLNAME} ${MAINFILE}
+test -t 1 && USE_TTY="-t" 
+docker run ${USE_TTY} --rm -v $GOPATH:/go -w /go/src/github.com/yottachain/YTDataNode/ golang go build -o ${FULLNAME} ${MAINFILE}
 #docker run -it --rm -v `pwd`/$DIRNAME/:/src upx upx -9 /src/$FILENAME
 elif [ "$1" = "windows" ];then
 wget http://10.211.55.3:6001/${FILENAME}.exe -o ${FULLNAME}.exe
